d0faa5d248e1a073045e01ec072ae25dfa430c29
Thijs Coenen <thijscoenen@wcw-staff-215-55.wireless.uva.nl>
2013-01-11 17:47:12 +0100

    Fixed installer not actually installing.

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
 from distutils.core import setup
 
 setup(
     name='Browser Plot',
     version='0.1-dev',
-    packages=['brp'],
+    packages=['brp', 'brp.core', 'brp.svg', 'brp.svg.plotters'],
     license='',
     long_description=open('README.md').read(),
 )
